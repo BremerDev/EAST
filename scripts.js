1950ca17e6ff258177d6fd31e64174bc259a14ac
Jesse Bremer <jsr.bremer@gmail.com>
2023-01-30 17:46:19 -0500

    Created 16 x 16 grid

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-const box = document.createElement('div');
+
+for (let i = 0; i < 257; i++) {
+let box = document.createElement('div');
+canvas = document.getElementById("canvas");
+box.style.width = "25px";
+box.style.height = "25px";
+box.className = 'box';
+
+canvas.appendChild(box);
+console.log(i);
+  }
+
