4d7adb10725047be9fe627917bd4900fcf70fd6a
Jesse Bremer <jsr.bremer@gmail.com>
2023-01-31 08:57:13 -0500

    Added event listener to add class for each 'pixel'.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 
 for (let i = 0; i < 257; i++) {
-let box = document.createElement('div');
+let box = document.createElement("div");
 canvas = document.getElementById("canvas");
 box.style.width = "25px";
 box.style.height = "25px";
-box.className = 'box';
+// box.className = "pixel";
+box.id = "pixel";
 
 canvas.appendChild(box);
 console.log(i);
   }
 
+
+document.getElementById("pixel").addEventListener("mouseover", changeBlack);
+
+  function changeBlack() {
+    document.getElementById("pixel").classList.add("black");
+  };
+  
