85e90227175bd50a1045a0b33eef51c939e69b52
Jesse Bremer <jsr.bremer@gmail.com>
2023-02-02 20:41:25 -0500

    Created loop to iterate through div nodes converted into an array.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
 
-for (let i = 0; i < 257; i++) {
+for (let i = 0; i < 256; i++) {
 let box = document.createElement("div");
 canvas = document.getElementById("canvas");
 box.style.width = "25px";
 box.style.height = "25px";
 // box.className = "pixel";
-box.id = "pixel";
+box.className = "pixel";
 
 canvas.appendChild(box);
 console.log(i);
   }
 
+// Grab all elements with the class name of pixel
+let getPixelClass = document.getElementsByClassName('pixel');
+console.log(getPixelClass);
 
-document.getElementById("pixel").addEventListener("mouseover", changeBlack);
+// Converted into an array
+let pixelArray = Array.from(getPixelClass);
+console.log(pixelArray);
 
-  function changeBlack() {
-    document.getElementById("pixel").classList.add("black");
-  };
-  
+//Loop through array with mouse over event listener
+for (let i = 0; i < pixelArray.length; i++) {
+    pixelArray[i].addEventListener("mouseover", function() {
+      pixelArray[i].style.backgroundColor = "black";
+    })};
