39c399fe05982670b9bf3f84dd88a51286ab4d0f
Jesse Bremer <jsr.bremer@gmail.com>
2023-02-02 20:43:27 -0500

    Refactored script file.

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ let box = document.createElement("div");
 canvas = document.getElementById("canvas");
 box.style.width = "25px";
 box.style.height = "25px";
-// box.className = "pixel";
 box.className = "pixel";
 
 canvas.appendChild(box);
@@ -13,11 +12,9 @@ console.log(i);
 
 // Grab all elements with the class name of pixel
 let getPixelClass = document.getElementsByClassName('pixel');
-console.log(getPixelClass);
 
 // Converted into an array
 let pixelArray = Array.from(getPixelClass);
-console.log(pixelArray);
 
 //Loop through array with mouse over event listener
 for (let i = 0; i < pixelArray.length; i++) {
