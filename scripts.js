e20f700ef7e5d6ffaceab6710610c51801db6a76
Jesse Bremer <jsr.bremer@gmail.com>
2023-02-02 21:14:17 -0500

    Working on button to take in proper prompt value.

--- scripts.js
+++ scripts.js
@@ -21,3 +21,25 @@ for (let i = 0; i < pixelArray.length; i++) {
     pixelArray[i].addEventListener("mouseover", function() {
       pixelArray[i].style.backgroundColor = "black";
     })};
+
+// Grab button
+const gridButton = document.getElementById("newGrid");
+
+// add an on click fuction to button
+gridButton.addEventListener('click', function() {
+    let gridSize =  prompt("Create a new grid between 1 - 100.");
+    if (isNaN(gridSize)) {
+        gridSize = prompt("Please choose a number between 1 - 100.");
+    } else if (gridSize > 100 || gridSize < 1) {
+            gridSize = prompt("Please choose a number between 1 - 100.");
+            console.log(gridSize);
+    } else {
+
+    }
+    
+}, false);
+
+
+//input cannot exceed 100 or letters, only numbers.
+
+
