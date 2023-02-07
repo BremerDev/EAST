4377c48d96f5883469a214e81e0f344906edf7fc
Jesse Bremer <jsr.bremer@gmail.com>
2023-02-07 15:47:41 -0500

    added second button that generate random colours on mouseover.

--- scripts.js
+++ scripts.js
@@ -27,9 +27,10 @@ for (let i = 0; i < pixelArray.length; i++) {
 
 colourBlack();
 
+
 // Grab button
 const gridButton = document.getElementById("newGrid");
-// add an on click fuction to button
+// add on click fuction to button
 gridButton.addEventListener('click', function() {
     //clear old grid
     while (canvas.hasChildNodes()) {
@@ -38,6 +39,7 @@ gridButton.addEventListener('click', function() {
 
     let gridSize =  prompt("Create a new grid between 1 - 100.");
 
+    //account for clicking cancel button
     if (isNaN(gridSize)) {
         gridSize = prompt("Please choose a number between 1 - 100.");
     } else if (gridSize > 100 || gridSize < 1) {
@@ -50,4 +52,56 @@ gridButton.addEventListener('click', function() {
     };
     
 }, false);
+
+
+
+// Grab button
+const rand = document.getElementById("randColour");
+// add on click fuction to button
+rand.addEventListener('click', function() {
+    //clear old grid
+    while (canvas.hasChildNodes()) {
+        canvas.removeChild(canvas.firstChild);
+      }
+
+    let gridSize =  prompt("Create a new grid between 1 - 100.");
+
+    //account for clicking cancel button
+    if (isNaN(gridSize)) {
+        gridSize = prompt("Please choose a number between 1 - 100.");
+    } else if (gridSize > 100 || gridSize < 1) {
+            gridSize = prompt("Please choose a number between 1 - 100.");
+    } else if (gridSize <= 100 && gridSize >= 1) {
+        makeRows(gridSize, gridSize);
+        
+            
+            let getPixelClass = document.getElementsByClassName('pixel');
+
+            let pixelArray = Array.from(getPixelClass);
+        
+            // random colour generator
+            
+               
+            
+            //Loop through array with mouse over event listener
+            for (let i = 0; i < pixelArray.length; i++) {
+                pixelArray[i].addEventListener("mouseover", function() {
+                    let r = Math.floor(Math.random()*256);
+                    let g = Math.floor(Math.random()*256);
+                    let b = Math.floor(Math.random()*256);
+                    let colour = "rgb(" + r + "," + g + "," + b + ")";
+                  pixelArray[i].style.setProperty('background-color', colour);
+                })};
+            
+        
+    } else {
+        console.log("It's not working.");
+    };
+    
+}, false);
+
+
+
+// Then try having each pass just add another 10% of black to it so that only after 
+// 10 passes is the square completely black.
 
